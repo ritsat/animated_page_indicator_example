1ed07bc33320dda3930dd92b266c2726aa8646c4
riteshchopade <ritsapptech@gmail.com>
2024-06-14 17:55:18 +0530

    Initial commit

---
+++ android/app/src/main/java/com/ecl/animated_page_indicator_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ecl.animated_page_indicator_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Animated Page Indicator Example",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: const Center(
+        child: Text("Abcd"),
+      ),
+    );
+  }
+}
